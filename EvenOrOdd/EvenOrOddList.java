bfa5023df390af85fe1337aadd62b6c2c7fd5fb6
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-16 23:17:58 +0100

    Refactor EvenOrOddList.java to include user input for number of numbers to check

---
+++ EvenOrOdd/EvenOrOddList.java
@@ -0,0 +1,9 @@
+package EvenOrOdd;
+import java.util.Scanner;
+public class EvenOrOddList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many numbers do you want to check : ");
+        int number = scanner.nextInt();
+ }
+}
