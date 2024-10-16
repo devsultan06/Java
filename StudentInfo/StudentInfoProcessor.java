5fe034f8c47f971735899f63d70394987a1a0119
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-16 22:57:10 +0100

    Refactor student info processing logic and add additional information
    This commit refactors the code in StudentInfoProcessor.java to improve the logic for processing student information. It also adds additional information such as age, birth year, GPA, and percentage. The code now prompts the user to enter their age and GPA, calculates the birth year based on the age, and calculates the percentage based on the GPA. The updated information is then displayed to the user. The commit also includes closing the scanner at the end of the code.

--- StudentInfo/StudentInfoProcessor.java
+++ StudentInfo/StudentInfoProcessor.java
@@ -7,7 +7,17 @@ System.out.print("Enter your first name: ");
 String firstname = scanner.nextLine();
 System.out.print("Enter your last name: ");
 String lastname = scanner.nextLine();
-
+System.out.print("Enter your age: ");
+int age = scanner.nextInt();
+System.out.print("Enter your GPA: ");
+double gpa = scanner.nextDouble();
+String fullname = firstname + " " + lastname;
+int birthYear = 2024-age;
+double percentage = (gpa/4.0) * 100;
+System.out.println("Fullname: " + fullname);
+System.out.println("Age: " + age + " , Birth Year: " + birthYear);
+System.out.println("GPA: " + gpa + " , Percentage: " + percentage + "%");
+scanner.close();
  }
     
 }
