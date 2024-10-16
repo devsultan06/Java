561dd80d1c73ca3cc12597c35ca04628c629e80a
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-16 22:45:29 +0100

    java

---
+++ StudentInfo/StudentInfoProcessor.java
@@ -0,0 +1,13 @@
+package StudentInfo;
+import java.util.Scanner;
+public class StudentInfoProcessor{
+ public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter your first name: ");
+String firstname = scanner.nextLine();
+System.out.print("Enter your last name: ");
+String lastname = scanner.nextLine();
+
+ }
+    
+}
