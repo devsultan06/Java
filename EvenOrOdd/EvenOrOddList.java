71f4a9a848ada21b265bf9927df9352fafffa013
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-16 23:26:03 +0100

    Refactor EvenOrOddList.java to include user input for number of numbers to check and display whether each number is even or odd

--- EvenOrOdd/EvenOrOddList.java
+++ EvenOrOdd/EvenOrOddList.java
@@ -1,9 +1,27 @@
 package EvenOrOdd;
+import java.util.Arrays;
 import java.util.Scanner;
 public class EvenOrOddList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many numbers do you want to check : ");
-        int number = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+          numbers[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+
+        for (int number : numbers) {
+           if (number % 2 == 0) {
+             System.out.println(number + " is even.");
+        } else {
+             System.out.println(number + " is odd.");
+         }
+    }
+
+
  }
 }
