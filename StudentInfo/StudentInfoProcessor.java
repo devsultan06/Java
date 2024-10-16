8d2e69ac75c89e40dafc8f92e377d47f09fda7b7
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-16 23:01:42 +0100

    Refactor student info processing logic and add additional information

--- StudentInfo/StudentInfoProcessor.java
+++ StudentInfo/StudentInfoProcessor.java
@@ -2,17 +2,25 @@ package StudentInfo;
 import java.util.Scanner;
 public class StudentInfoProcessor{
  public static void main(String[] args){
+// Creating a scanner object
 Scanner scanner = new Scanner(System.in);
+// Prompt for First Name
 System.out.print("Enter your first name: ");
 String firstname = scanner.nextLine();
+// Prompt for Last Name
 System.out.print("Enter your last name: ");
 String lastname = scanner.nextLine();
+// Prompt for Age
 System.out.print("Enter your age: ");
 int age = scanner.nextInt();
+// Prompt for GPA
 System.out.print("Enter your GPA: ");
 double gpa = scanner.nextDouble();
+// Combination of First Name and Last Name to a string
 String fullname = firstname + " " + lastname;
+//Calculate the birth year(2024-age)
 int birthYear = 2024-age;
+//Converting the GPA to a percentage 
 double percentage = (gpa/4.0) * 100;
 System.out.println("Fullname: " + fullname);
 System.out.println("Age: " + age + " , Birth Year: " + birthYear);
